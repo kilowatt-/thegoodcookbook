0bf4853501c81061bac5e3f12ebbb9370051747c
kilowatt- <wongkeanwah@gmail.com>
2019-06-03 14:59:40 -0700

    Added Difficulty and FoodType enums

---
+++ imports/ui/components/Difficulty.js
@@ -0,0 +1,5 @@
+export default const Difficulty = {
+	EASY: 'easy',
+	MEDIUM: 'medium',
+	HARD: 'hard'
+}
---
+++ imports/ui/components/FoodType.js
@@ -0,0 +1,7 @@
+export default const FoodType = {
+	BREAKFAST: 'breakfast',
+	LUNCH: 'lunch',
+	DINNER: 'dinner',
+	SNACK: 'snack',
+	DESSERT: 'dessert'
+}
