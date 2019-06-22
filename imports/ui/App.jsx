992df95963eee3cdbcab71bff4ceb4744c9563c9
Ryan Knauer <ryanknauer@alumni.ubc.ca>
2019-06-21 22:23:03 -0700

    added filters

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,11 +3,13 @@ import RecipeForm from './components/RecipeForm.jsx';
 import RecipeCards from './components/RecipeCards.jsx';
 import PostRecipeButton from './components/PostRecipeButton.jsx'
 import SearchBar from './components/SearchBar.jsx'
+import FilterBar from './components/FilterBar.jsx';
 
 const App = () => (
   <div>
     <h1>the good cook book</h1>
     <SearchBar />
+    <FilterBar />
     <PostRecipeButton />
     <RecipeCards />
   </div>
