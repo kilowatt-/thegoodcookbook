58acb0903592dfeb7eb6b4b0a03c6b3787612113
kilowatt- <wongkeanwah@gmail.com>
2019-06-27 22:29:05 -0700

    Added login button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,11 +4,13 @@ import RecipeCards from './components/RecipeCards.jsx';
 import PostRecipeButton from './components/PostRecipeButton.jsx'
 import SearchBar from './components/SearchBar.jsx'
 import FilterBar from './components/FilterBar.jsx';
-import RandomRecipeButton from './components/RandomRecipeButton.jsx'
+import RandomRecipeButton from './components/RandomRecipeButton.jsx';
+import LoginButton from './components/LoginButton.jsx'
 
 const App = () => (
   <div>
     <h1>the good cook book</h1>
+    <LoginButton />
     <SearchBar />
     <FilterBar />
     <PostRecipeButton />
