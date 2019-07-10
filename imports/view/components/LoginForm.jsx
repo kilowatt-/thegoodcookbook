8167168af4a68d9e4a4e031b3a0c556e21bff208
kilowatt- <wongkeanwah@gmail.com>
2019-07-10 10:27:00 -0700

    removed accounts-ui, also changed username field to email

--- imports/view/components/LoginForm.jsx
+++ imports/view/components/LoginForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Blaze from 'meteor/gadicc:blaze-react-component';
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import Button from '@material-ui/core/Button';
 import { Meteor } from 'meteor/meteor';
@@ -19,7 +18,7 @@ class LoginForm extends React.Component {
 		this.logOut = this.logout.bind(this);
 
 		this.state = {
-			username: '',
+			email: '',
 			password: '',
 			error: '',
 			registering: false
@@ -42,7 +41,7 @@ class LoginForm extends React.Component {
 	handleSubmit(event) {
 		event.preventDefault();
 
-		Meteor.loginWithPassword(this.state.username, this.state.password, 
+		Meteor.loginWithPassword(this.state.email, this.state.password, 
 		(err) => {
 			if (err) {
 				this.setState({
@@ -51,7 +50,7 @@ class LoginForm extends React.Component {
 			}
 			else {
 				this.setState({
-					username: '',
+					email: '',
 					password: '',
 					error: '',
 					registering: false
@@ -86,13 +85,13 @@ class LoginForm extends React.Component {
 
 		return (
 			<div className="login">
-			<Blaze template="loginButtons" />
+
 			{this.props.user ? <span>Hello, {Meteor.user().name}! (<a href="#" onClick={this.logout}>Logout</a>)  </span> :
 
 			<div>
 
 			<ValidatorForm onSubmit={this.handleSubmit}>
-				<TextValidator className="tf_username" validators={['required']} errorMessages={['Required']} id="username" name="username"  value={this.state.username} onChange={this.handleChange} variant="filled" label="Username" />
+				<TextValidator className="tf_email" validators={['required'], ['isEmail']} errorMessages={['Required'], ['Enter a valid email']} id="email" name="email"  value={this.state.email} onChange={this.handleChange} variant="filled" label="Email" />
 				<TextValidator className="tf_password" validators={['required']} errorMessages={['Required']} id="password" name="password"  value={this.state.password} type='password' onChange={this.handleChange} variant="filled" label="Password" />
 				<Button type="submit" className="bt_login">Login</Button><br />
 				<a href="#" onClick={this.openRegistrationForm}>Register</a> <span id="login_error_msg">{this.state.error}</span>
