a41a455d666c77da215f19bfadb66bdbfad5e724
Ryan Knauer <ryanknauer@alumni.ubc.ca>
2019-08-06 16:08:06 -0700

    clear chip search on home page

--- imports/controller/reducers/inputReducer.js
+++ imports/controller/reducers/inputReducer.js
@@ -1,6 +1,10 @@
+import {BROWSE_ADDED, BROWSE_ALL, BROWSE_FAVORITES, HOME} from '../actions/navBar.js';
 const DEFAULT_INPUT = {searchType:'Name'};
 
 export const inputReducer = (inputs = DEFAULT_INPUT, action) => {
+	if(action.type === HOME) {
+		return {...inputs, chipSearch:[] };
+	}
 	if (action.type === "UPDATE_INPUT") {
 		const field = action.payload[0];
 		const value = action.payload[1];
